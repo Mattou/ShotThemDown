6ee0e216512cf34397afa3c03da00c7f913628e9
Mattou <mathilde.lplz@gmail.com>
2017-11-15 20:01:47 +0100

    Add collision on left wall and tile on starting

--- game/graphic.js
+++ game/graphic.js
@@ -46,9 +46,11 @@ function Ground(color, size_x, size_y, nb_tile)
 
     for (x = minX; x <= maxX; x = x+sizeOfTileX){
         for (y = minY; y <= maxY; y = y+sizeOfTileY){
-
-            color = colors[Math.floor(Math.random()*colors.length)];
-       
+            if (x == 0 && y == 0) {
+                color = 0x00ff00;
+            } else {
+                color = colors[Math.floor(Math.random()*colors.length)];
+            }
             if (0x000000 != color)
             {
                 tmpGround = new THREE.Mesh(
